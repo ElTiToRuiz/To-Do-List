ce791debd268fc91c9365cc98d63f7b4add099b5
Igor Ruiz <ruizigor16@gmail.com>
2024-06-07 00:29:28 +0200

    setup todolist project

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+// Add your JavaScript here
+document.addEventListener('DOMContentLoaded', () => {
+  // Your logic here
+  const app = document.getElementById('app');
+  app.innerHTML = '<h1>To-Do List</h1>';
+});
